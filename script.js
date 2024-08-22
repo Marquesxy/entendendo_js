7735e23dbe2f5a18c4d268f55b5bf6ae9f61b185
Marquesxy <mairon.msantos@senacsp.edu.br>
2024-08-21 22:13:48 -0300

    Exercicios 22/08

---
+++ script.js
@@ -0,0 +1,37 @@
+/*
+Ex1: Informe se um aluno está aprovado,
+de recuperação ou reprovado
+seguindo os seguintes critérios:
+informar 3 notas.
+media > 6: aprovado.
+media < 4: reprovado.
+ 
+Obs use o prompt e exiba os resultado em alert.
+ 
+*/
+const MediadaProva = 6;
+const MediadaProva1 = 4;
+
+let nome
+let n1, n2, n3 = 0;
+let mensagem
+
+media = (n1 + n2 + n3 / 3);
+
+nome = prompt('informe o nome do aluno: ');
+
+n1 = parseFloat(prompt ('Informe a primeira nota obtida do aluno');
+n2 = parseFloat(prompt ('Informe a segunda nota obtida do aluno');
+n3 = parseFloat(prompt ('Informe a trceira nota obtida do aluno');
+
+let media = 10;
+if (media >= 6) {
+    console.log("O aluno esta aprovado");
+ else  (media <= 4) 
+    mensagem("O aluno esta Reprovado");
+}
+
+
+ 
+
+
